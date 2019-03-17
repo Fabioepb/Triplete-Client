1c221e73044bef80b3647e44d4ce21dc0af5d0dd
djfabiokk@gmail.com <djfabiokk@gmail.com>
2019-03-17 16:38:11 -0400

    Reworked Homepage, theme change

---
+++ public/README.md
@@ -0,0 +1,17 @@
+# Triplete
+Esta es una aplicacion web creada para la asignatura Ingenieria de Software II, diseñada para que los clientes de un negocio de apuestas puedan realizarlas en linea, ver estadisticas sobre los partidos, tener un perfil y entre otras cosas.
+
+This is a web app developed for our Software Engineering II asignment, the app's purpose is to let the clients of a betting company place their bets online, see match statistics, have a unique profile and more.
+
+## Tecnologias Usadas / Technologies used
+* HTML5
+* CSS3
+* Bootstrap 4
+* Nodejs
+* ExpressJs
+
+## Guia de instalacion / Installation guide
+```
+npm install
+npm start
+```
--- public/components/navbar.js
+++ public/components/navbar.js
@@ -1,6 +1,5 @@
 function LogOut() {
-    let path = document.location.pathname;
-    //let url = (path == '/views/') ? './Logout' : '../Logout';
+    //let path = document.location.pathname;
     let configs = {
         method: 'GET',
         headers: {
@@ -9,7 +8,7 @@ function LogOut() {
         credentials: 'same-origin'
     }
    
-    fetch(url, configs)
+    fetch('localhost:8080/logout', configs)
         .then(response => response.json())
         .then(data => {        
             console.log("Logged out")             
@@ -38,7 +37,7 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
         <nav style="background-color:#262626;"
         class="navbar navbar-expand-md navbar-dark justify-content-between">
-            <a class="navbar-brand" style="font-size: 20px !important">Triple Jonron</a>
+            <a class="navbar-brand" style="font-size: 20px !important; margin-left: 5%;">Triplete</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#cContent" aria-controls="cContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>            
@@ -51,6 +50,7 @@ class NavBar extends HTMLElement {
     } 
     userState() {
         let userData = this.userData;
+
         //let userData ={
         //    username: "GeometriaEnEl8vo",
         //}
@@ -69,14 +69,14 @@ class NavBar extends HTMLElement {
                     <a class="navbar-text mt-1" style="color: white; font-size: 18px">Bienvenido, </a>
                     <b class="navbar-text mr-5 ml-2 mt-1" style="color: white; font-size: 18px" >${name}</b>
                 </li>
-                <li class="nav-item" style="border-right-style: solid; border-right-color: #E1AF39; border-right-width: 1px; border-right-height: 1px;">
-                    <a class=" nav-link" href="${this.urls.bet}" style="color: #E1AF39; font-size: 20px">Apostar</a>
+                <li class="nav-item" style="border-right-style: solid; border-right-color: #689f38; border-right-width: 1px; border-right-height: 1px;">
+                    <a class=" nav-link" href="${this.urls.bet}" style="color: #689f38; font-size: 20px">Apostar</a>
                 </li>
-                <li class="nav-item" style="border-right-style: solid; border-right-color: #E1AF39; border-right-width: 1px; border-right-height: 1px;">
-                    <a id="id1" class="nav-link" href="${this.urls.profile}" style="color: #E1AF39; font-size: 20px">Perfil</a>
+                <li class="nav-item" style="border-right-style: solid; border-right-color: #689f38; border-right-width: 1px; border-right-height: 1px;">
+                    <a id="id1" class="nav-link" href="${this.urls.profile}" style="color: #689f38; font-size: 20px">Perfil</a>
                 </li>
-                <li class="nav-item" style="border-right-style: solid; border-right-color: #E1AF39; border-right-width: 1px; border-right-height: 1px;">
-                    <a id="id1" class="nav-link" href="#" style="color: #E1AF39; font-size: 20px" onClick="LogOut()">Cerrar Sesion</a>
+                <li class="nav-item" style="border-right-style: solid; border-right-color: #689f38; border-right-width: 1px; border-right-height: 1px;">
+                    <a id="id1" class="nav-link" href="#" style="color: #689f38; font-size: 20px" onClick="LogOut()">Cerrar Sesion</a>
                 </li>
             </ul>
         `
@@ -89,11 +89,11 @@ class NavBar extends HTMLElement {
                 <a class="navbar-text mt-1" style="color: white; font-size: 18px">Bienvenido, </a>
                 <b class="navbar-text mr-5 ml-2 mt-1" style="color: white; font-size: 18px">invitado</b>
             </li>
-            <li class="nav-item" style="border-right-style: solid; border-right-color: #E1AF39; border-right-width: 1px; border-right-height: 1px;">
-                <a class=" nav-link" href="${this.urls.login}" style="color: #E1AF39; font-size: 20px">Iniciar Sesion</a>
+            <li class="nav-item" style="border-right-style: solid; border-right-color: #689f38; border-right-width: 1px; border-right-height: 1px;">
+                <a class=" nav-link" href="${this.urls.login}" style="color: #689f38; font-size: 20px">Iniciar Sesion</a>
             </li>
-            <li class="nav-item" style="border-right-style: solid; border-right-color: #E1AF39; border-right-width: 1px; border-right-height: 1px;">
-                <a id="id1" class="nav-link" href="${this.urls.signup}" style="color: #E1AF39; font-size: 20px">Registrarse</a>
+            <li class="nav-item" style="border-right-style: solid; border-right-color: #689f38; border-right-width: 1px; border-right-height: 1px;">
+                <a id="id1" class="nav-link" href="${this.urls.signup}" style="color: #689f38; font-size: 20px">Registrarse</a>
             </li>
         </ul>
         `
